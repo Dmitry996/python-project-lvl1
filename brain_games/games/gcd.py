d520ab2395eb9eb475b7f804e3d0b3ff74055ac7
Dmitry Rodin <dimoon-96@yandex.ru>
2022-03-06 22:19:27 +0700

    0.1.0

---
+++ brain_games/games/gcd.py
@@ -0,0 +1,14 @@
+from random import randint
+from math import gcd
+
+GAMECONDITION = 'Find the greatest common divisor of given numbers.'
+
+
+def qwest_res():
+    a, b = randint(1, 50), randint(1, 50)
+    print(f'Question: {a} {b}')
+    user_answer = int(input('You answer: '))
+    answer = gcd(a, b)
+    return user_answer, answer
+
+
---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,10 @@
+from brain_games.games import gcd
+from brain_games.engine import engine
+
+
+def main():
+    engine(gcd)
+
+
+if __name__ == '__main__':
+    main()
