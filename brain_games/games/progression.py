12ee3dc0f575560a183a99a1b65fefefd98d46a7
Dmitry Rodin <dimoon-96@yandex.ru>
2022-03-10 22:58:48 +0700

    0.1.0

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -4,11 +4,11 @@ GAME_CONDITION = 'What number is missing in the progression?'
 
 
 def get_round():
-    step = randint(3, 10)
-    start = randint(1, 50)
-    numbers = [str(i) for i in range(start, start + (step * 10), step)]
-    index = randint(0, 9)
-    answer = numbers[index]
-    numbers[index] = '..'
+    STEP = randint(3, 10)
+    START = randint(1, 50)
+    numbers = [str(i) for i in range(START, START + (STEP * 10), STEP)]
+    INDEX = randint(0, 9)
+    answer = numbers[INDEX]
+    numbers[INDEX] = '..'
     quest = ' '.join(numbers)
     return answer, quest
