81a732388af1fa053e157c6602bfcd5b82879ed2
Dmitry Rodin <dimoon-96@yandex.ru>
2022-03-10 23:09:57 +0700

    0.1.0

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -5,7 +5,7 @@ GAME_CONDITION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 def is_prime(num):
     count = 0
-    for i in range(1, int(num / 2) + 1):
+    for i in range(1, num // 2 + 1):
         if num % i == 0:
             count += 1
         if count > 2:
